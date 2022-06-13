957fa104cf91e143b1a7afa4bf32bc507216aceb
laumags <laumadz@gmail.com>
2022-06-13 20:42:30 +0300

    first homework

--- loops/8.php
+++ loops/8.php
@@ -1,6 +1,12 @@
 <?php
 $min = (int) readline ('Enter min: ');
 $max = (int) readline ('Enter max: ');
-for ($i = $min; $min <= $max; $min++) {
-    echo $min;
+for ($i = $min; $i <= $max; $i++) {
+    $number = range($i, $max);
+    $sequence = range($min, ($i - 1));
+    if (count($number) == $max - $min + 1) {
+        echo implode('', $number) . PHP_EOL;
+    } else {
+        echo implode('', $number) . implode('', $sequence) . PHP_EOL;
+    }
 }
